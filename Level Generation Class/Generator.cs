9cca020156e91684a3c78b94e71a40d413779f3d
Sam Phillips <Sam Phillips>
2016-03-23 01:02:02 +0000

    Initial commit.

---
+++ Level Generation Class/Generator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_Generation_Class
+{
+    class Generator
+    {
+        // Private Variables
+        private int _seed;
+        private SubtractiveGenerator gen;
+
+        // Public Variables
+        public int Seed {
+            get { return _seed; }
+            set { _seed = value; }
+        }
+
+        public Generator() {
+            _seed = (int)(new Random().Next(0, int.MaxValue) + int.MaxValue);
+            Init();
+        }
+
+        public Generator(int seed) {
+            _seed = seed;
+            Init();
+        }
+
+        private void Init() {
+            // Initialize class
+            gen = new SubtractiveGenerator(_seed);
+        }
+
+        public int[] GetSequence1D(int length, int minValue, int maxValue) {
+            int[] result = new int[length];
+            int range = maxValue - minValue;
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = (int)((double)gen.next() / (double)SubtractiveGenerator.MAX * (double)range) + minValue;
+            }
+            return result;
+        }
+
+        public int[,] GetSequence2D(int length, int width, int minValue, int maxValue) {
+            int[,] result = new int[length, width];
+            int range = maxValue - minValue;
+
+            for (int l = 0; l < length; l++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    result[l, w] = (int)((double)gen.next() / (double)SubtractiveGenerator.MAX * (double)range) + minValue;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Level Generation Class/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Level_Generation_Class
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Generator g;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            g = new Generator();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int[] result = g.GetSequence1D(20, 400, 1200);
+
+            foreach (int i in result) {
+                Console.Write(i + ", ");
+            }
+        }
+
+        private void btnGenerate2D_Click(object sender, RoutedEventArgs e)
+        {
+            int[,] result = g.GetSequence2D(10, 10, 0, 10);
+
+            for (int l = 0; l < result.GetLength(0); l++)
+            {
+                for (int w = 0; w < result.GetLength(1); w++)
+                {
+                    Console.Write(result[l,w] + ", ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
